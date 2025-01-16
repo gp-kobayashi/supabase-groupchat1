055f803a01d5d1572a4d64a3c75e73d422318c32
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-01-16 15:33:53 +0900

    [add]supabaseを操作する関数の追加

---
+++ app/utils/supabase_function.ts
@@ -0,0 +1,61 @@
+import { supabase } from "./supabase";
+import type { Database } from "@/lib/database.types";
+
+type Group = Database["public"]["Tables"]["groups"]["Row"];
+type Chat = Database["public"]["Tables"]["chats"]["Row"];
+
+type SupabaseResponse<T> = {
+    data: T | null;
+    error: Error | null;
+  };
+
+export const GetGroupList = async ():Promise<SupabaseResponse<Group[]>> => {
+    const { data, error } = await supabase
+        .from('groups')
+        .select('*');
+    if (error) {
+        return {data: null, error};
+    }
+    return {data, error: null};
+}
+
+export const CreateGroup = async (
+        title: string
+    ):Promise<SupabaseResponse<Group>> => {
+        const { data, error } = await supabase
+            .from('groups')
+            .insert({ title })
+            .select()
+            .single();
+        return {data, error};
+}; 
+
+export const GetChatList = async (
+    groupId: number
+    ) :Promise<SupabaseResponse<Chat[]>>=> {
+        const { data, error } = await supabase
+            .from('chats')
+            .select('*')
+            .eq("group_id",groupId);
+        if(error){
+            return {data: null, error};
+        }
+    return {data, error: null};
+}
+
+export const AddChat = async (
+        groupId: number,
+        userId: string,
+        text: string
+    ):Promise<SupabaseResponse<Chat>> => {
+        const { data, error } = await supabase
+            .from('chats')
+            .insert({
+                group_id: groupId,
+                user_id: userId,
+                text: text
+            })
+            .select()
+            .single();
+        return {data, error};
+};
