802e979d6e9757bbeae7ecf9978c323232793fdd
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-01-17 13:46:31 +0900

    [add]チャットのグループ機能の追加

--- app/components/group-chat/groupApp.tsx
+++ app/components/group-chat/groupApp.tsx
@@ -1,12 +1,14 @@
-import { useState,useEffect } from 'react';
+"use client";
+
+import { useState,useEffect,useCallback } from 'react';
 import { Database } from '@/lib/database.types';
 import GroupList  from './groupList';
-import { GetGroupList } from '@/app/utils/supabase_function';
+import { GetGroupList,CreateGroup } from '@/app/utils/supabase_function';
 
 
 const GroupApp = () => {
     const [groupList, setGroupList] = useState<Database["public"]["Tables"]["groups"]["Row"][]>([]);
-    const [title, setTitle]= useState("");
+    const [title, setTitle]= useState<string>("");
     const [message, setMessages] = useState("");
 
     useEffect (() => {
@@ -19,14 +21,34 @@ const GroupApp = () => {
             setGroupList(data || []);
         }
         groupList();
-    },[])
+    },[groupList])
+
+    const handleSubmit = useCallback(
+        async(e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+            if(title === "")return;
+            const {data:updatedGroupList, error } = await CreateGroup(title);
+
+            if(error){
+                setMessages("エラーが発生しました"+error.message);
+                return;
+            }
+            if(updatedGroupList){
+                setGroupList((prevGroupList)=>[...prevGroupList, updatedGroupList]);
+            }
+            setTitle("");
+            setMessages("");
+        },[title, groupList],
+    );
 
     return(
         <div>
-            <form >
+            <form onSubmit={(e) => handleSubmit(e)}>
                 <input
                     type="text"
                     placeholder="グループ名"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
                 />
                 <button>
                     グループ作成
--- app/components/group-chat/groupList.tsx
+++ app/components/group-chat/groupList.tsx
@@ -1,32 +1,36 @@
 "use client";
 
-import { useCallback,useState, SetStateAction } from "react";
+import { useCallback,useState, SetStateAction } from "react"
+import style from "./groupchat.module.css";
 import { Database } from "@/lib/database.types";
 import { GetChatList } from "@/app/utils/supabase_function";
+import ChatApp from "./chatApp";
 
 type Props = {
     groupList: Database["public"]["Tables"]["groups"]["Row"][];
     setGroupList: React.Dispatch<SetStateAction<Database["public"]["Tables"]["groups"]["Row"][]>>;
 }
 
-const GroupList = ({ Props:Props }) => {
-
-    const {groupList, setGroupList} = Props;
-
+const GroupList = (props:Props) => {
+    const {groupList, setGroupList} = props;
     return(
         <div>
             <ul>
                 {groupList.map((group) => (
-                    <li key={groupList.id}>
-                        <div>{groupList.title}</div>
-                        <button onClick={() => GetChatList(groupList.id)}>
+                    <li key={group.id}>
+                        <div>{group.title}</div>
+                        <button onClick={() => GetChatList(group.id)}>
                         参加
                         </button>
                     </li>
                 ))
                 }
             </ul>
+            <div>
+                <ChatApp />
+            </div>
         </div>
+        
     )
 }
 
