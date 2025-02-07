f3796377ebb4b0b11f9640940f9f50cd7d673069
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-02-07 13:38:15 +0900

    [fix]ドメインを.env.localから取得

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+if (!supabaseUrl) {
+  throw new Error("env.NEXT_PUBLIC_SUPABASE_URLが設定されていません");
+}
+const url = new URL(supabaseUrl);
+const domain = url.hostname;
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['qdwzilkzjmmecqlfvtbq.supabase.co'], // ここに外部ホストを追加
+    domains: [domain], 
   },
 };
 
