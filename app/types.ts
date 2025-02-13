f846cc84082ae107c22d8a44622cf11b2c8a6bc0
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-02-13 13:30:45 +0900

    [clean]ChatWithAvatarの型定義を交差型に

--- app/types.ts
+++ app/types.ts
@@ -8,12 +8,4 @@ export type Group = Database["public"]["Tables"]["groups"]["Row"];
 
 export type Avatar_url = Profile["avatar_url"];
 
-export type ChatWithAvatar = {
-  create_at: Chat["create_at"];
-  group_id: Chat["group_id"];
-  id: Chat["id"];
-  profiles: { avatar_url: Profile["avatar_url"] };
-  text: Chat["text"];
-  user_id: Chat["user_id"];
-  update_at: Chat["update_at"];
-};
+export type ChatWithAvatar = Chat & { profiles: Avatar_url[] };
