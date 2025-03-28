b693cefbe622f8653a8a73aac276546827a08280
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-03-28 14:31:44 +0900

    [add]グループ参加者のみにチャットの読み取りを許可するRLS

--- sql/rls.sql
+++ sql/rls.sql
@@ -1,12 +1,21 @@
--- chatsにRLSを適用する
 ALTER TABLE chats ENABLE ROW LEVEL SECURITY;
--- chatsにInsertを許可するポリシーを作成する
 CREATE POLICY "Insert messages only if user is active in group"
 ON chats
 FOR INSERT
--- チェック項目を記述
 WITH CHECK (
-  -- 今回はexistsで条件を満たすレコードがあるかをチェック
+  EXISTS (
+    SELECT 1 FROM group_members
+    WHERE group_members.user_id = auth.uid()
+      AND group_members.group_id = chats.group_id
+      AND group_members.status = 'active'
+  )
+);
+
+ALTER TABLE chats ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "Select messages only if user is active in group"
+ON chats
+FOR SELECT
+USING (
   EXISTS (
     SELECT 1 FROM group_members
     WHERE group_members.user_id = auth.uid()
