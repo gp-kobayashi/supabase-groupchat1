d22271d28dc1e7110caa06027e072df8bab2856c
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-01 14:54:14 +0900

    [fix]utils/supabaseのclient.ts,server.tsにそれぞれ型定義を追加

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,6 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
+import { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "@/lib/database.types";
 
-export function createClient() {
+export function createClient(): SupabaseClient<Database> {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
