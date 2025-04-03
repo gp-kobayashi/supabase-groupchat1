603f3abaea9f9af5617d5255c4354888f78e7576
gp-kobayashi <kobayashi_y@gp1000.jp>
2025-04-03 15:06:37 +0900

    [fix]型定義修正によるエラーの回避

--- app/account/account-form.tsx
+++ app/account/account-form.tsx
@@ -17,6 +17,7 @@ export default function AccountForm({ user }: { user: User | null }) {
     try {
       setLoading(true);
 
+      if (!user?.id) return;
       const { data, error, status } = await supabase
         .from("profiles")
         .select(`full_name, username, website, avatar_url`)
